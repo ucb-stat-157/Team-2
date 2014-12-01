80d867d5e62b964dcc60e818ab9c8cbc7fe9cf12
cptincredble <cptincredble@gmail.com>
2014-12-01 14:46:51 -0800

    add mse for regression

--- code/regression_sklearn.py
+++ code/regression_sklearn.py
@@ -9,7 +9,7 @@ values = []
 
 for instance in instances:
     instance = instance.split('\t')
-    print instance
+    # print instance
 
 
     if int(instance[2]) != 0:
@@ -48,54 +48,17 @@ for test in tests:
 
 print "countz"
 print right
-
-
-
 print total
 
-# source: http://www.kddcup2012.org/c/kddcup2012-track2/forums/t/1776/understanding-the-auc
-def scoreClickAUC(num_clicks, num_impressions, predicted_ctr):
-    """
-    Calculates the area under the ROC curve (AUC) for click rates
 
-    Parameters
-    ----------
-    num_clicks : a list containing the number of clicks
+print "mse"
+print len(values)
+print len(ctrs)
+print len(clicks)
+mse = 0
+for index in range(len(ctrs)):
+    actual = float(clicks[index]) / impressions[index]
+    mse += (actual - ctrs[index])**2
 
-    num_impressions : a list containing the number of impressions
+print mse/len(values)
 
-    predicted_ctr : a list containing the predicted click-through rates
-
-    Returns
-    -------
-    auc : the area under the ROC curve (AUC) for click rates
-    """
-    i_sorted = sorted(range(len(predicted_ctr)),key=lambda i: predicted_ctr[i],
-                      reverse=True)
-    auc_temp = 0.0
-    click_sum = 0.0
-    old_click_sum = 0.0
-    no_click = 0.0
-    no_click_sum = 0.0
-
-    # treat all instances with the same predicted_ctr as coming from the
-    # same bucket
-    last_ctr = predicted_ctr[i_sorted[0]] + 1.0
-
-    for i in range(len(predicted_ctr)):
-        if last_ctr != predicted_ctr[i_sorted[i]]:
-            auc_temp += (click_sum+old_click_sum) * no_click / 2.0
-            old_click_sum = click_sum
-            no_click = 0.0
-            last_ctr = predicted_ctr[i_sorted[i]]
-        no_click += num_impressions[i_sorted[i]] - num_clicks[i_sorted[i]]
-        no_click_sum += num_impressions[i_sorted[i]] - num_clicks[i_sorted[i]]
-        click_sum += num_clicks[i_sorted[i]]
-    auc_temp += (click_sum+old_click_sum) * no_click / 2.0
-    auc = auc_temp / (click_sum * no_click_sum)
-    return auc
-
-print scoreClickAUC(clicks, impressions, ctrs)
-
-
-print metrics.auc(values, ctrs)
