7343cc43d725e92922d9ae190e068d05d712e288
cptincredble <cptincredble@gmail.com>
2014-12-01 23:34:28 -0800

    remove some junk

--- code/create_test_data.py
+++
@@ -1,9 +0,0 @@
-import random
-
-indices = random.sample(xrange(4051441), 10000)
-
-users = [line.strip() for line in open('titleid_tokensid.txt')]
-
-with open("test_title.txt", "a") as myfile:
-    for index in indices:
-        myfile.write(users[index] + '\n')
