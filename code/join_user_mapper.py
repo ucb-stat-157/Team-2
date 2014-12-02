14d0442b6f8963a303108682474bc862062fea80
cptincredble <cptincredble@gmail.com>
2014-12-01 23:44:19 -0800

    add feature specific joins, rename unclear joins

---
+++ code/join_user_mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+len_side = 0
+instance_index = 0
+
+for line in sys.stdin:
+    line = line.strip()
+
+    train_or_user = line.split('\t')
+    if len(train_or_user) == 3:
+        print line
+    if len(train_or_user) == 12:
+        print '%s\t%s' % (train_or_user[11], line)
