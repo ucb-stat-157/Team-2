5ee859263a1cd5334c074451815eea611ed92ccc
cptincredble <cptincredble@gmail.com>
2014-12-05 15:47:50 -0800

    some sklearn stuff

--- code/sklearn_qual.py
+++ code/sklearn_qual.py
@@ -1,6 +1,13 @@
+### COMPLETELY NOT IN USE ###
+
+# Testing using the sklearn package
+# Requires sampled data which I don't have anymore
+
 from sklearn import tree
 from sklearn import svm
 from sklearn.metrics import roc_curve, auc
+from sklearn import linear_model
+from sklearn.naive_bayes import BernoulliNB
 
 import ml_metrics as metrics
 
@@ -11,36 +18,26 @@ values = []
 
 for instance in instances:
     instance = instance.split('\t')
-    # print instance
-    sample = [instance[12], instance[13], instance[6]]
+    sample = [int(instance[12]), int(instance[13]), int(instance[6])]
+    print sample
     samples.append(sample)
 
-    # print "sample"
-    # print sample
     if instance[0] != '0':
-        # print 1
         values.append(1)
     else:
-        # print 0
         values.append(0)
-
-# spoof
-# for i in range(1000):
-#     s = [1, 3, 3]
-#     samples.append(s)
-#     values.append(1)
-
-# for i in range(1000):
-#     s = [2, 2, 2]
-#     samples.append(s)
-#     values.append(1)
-
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(samples, values)
 
 clf2 = svm.SVC()
 clf2.fit(samples, values)
+
+bnb = BernoulliNB()
+bnb.fit(samples, values)
+
+logreg = linear_model.LogisticRegression(C=1e5)
+logreg.fit(samples, values)
 
 tests = [line.strip() for line in open('combined_instances.txt')]
 
@@ -63,7 +60,7 @@ ctrs2 = []
 for test in tests:
     test = test.split('\t')
 
-    sample = [test[12], test[13], test[6]]
+    sample = [int(test[12]), int(test[13]), int(test[6])]
     clicks.append(int(test[0]))
     impressions.append(int(test[1]))
     depth.append(int(test[2]))
@@ -81,28 +78,11 @@ for test in tests:
     ctrs2.append(float(val))
 
     if int(val[0]) == int(test[0]):
-        if int(val[0]) == 1:
-            print 'predicted click'
         right += 1
 
     if int(val2[0]) == int(test[0]):
-        if int(val2[0]) == 1:
-            print 'predicted click'
         right2 += 1
-    # else:
-    #     print 'predicted'
-    #     print val[0]
-    #     print 'actual'
-    #     print test[0]
     total += 1
-
-print "countz"
-print right
-print right2
-
-
-
-print total
 
 # source: http://www.kddcup2012.org/c/kddcup2012-track2/forums/t/1776/understanding-the-auc
 def scoreClickAUC(num_clicks, num_impressions, predicted_ctr):
@@ -149,32 +129,3 @@ def scoreClickAUC(num_clicks, num_impressions, predicted_ctr):
 print scoreClickAUC(clicks, impressions, ctrs)
 
 print scoreClickAUC(clicks2, impressions2, ctrs2)
-
-
-binary_click = []
-for click in clicks:
-    if int(click) != 0:
-        binary_click.append(1.0)
-    else:
-        binary_click.append(0.0)
-
-
-print metrics.auc(ctrs, binary_click)
-# print metrics.auc(binary_click, binary_click)
-# print binary_click
-
-# print "wtf"
-# print metrics.auc(ctrs, ctrs)
-
-# print binary_click
-# print ctrs
-
-
-
-fpr, tpr, _ = roc_curve(binary_click, ctrs)
-print "auc"
-# print fpr
-# print type(fpr)
-# print tpr
-print auc(fpr, tpr)
-print auc(tpr, tpr)
